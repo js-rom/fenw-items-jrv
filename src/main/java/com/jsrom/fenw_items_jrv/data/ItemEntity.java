c0b416e06d98c93fe2fa7b5762802291a9ca2847
[jesus romero] <jesus.romerovidal@gmail.com>
2025-04-13 14:10:48 +0200

    add swagger

--- src/main/java/com/jsrom/fenw_items_jrv/data/ItemEntity.java
+++ src/main/java/com/jsrom/fenw_items_jrv/data/ItemEntity.java
@@ -1,14 +1,15 @@
 package com.jsrom.fenw_items_jrv.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Entity
+
+@Entity()
 public class ItemEntity {
 
-    @Id()
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
