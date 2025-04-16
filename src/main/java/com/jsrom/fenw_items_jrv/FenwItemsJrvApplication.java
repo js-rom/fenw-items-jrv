3d97134caa0bf95ed17a957eb19eb56e2ca633df
[jesus romero] <jesus.romerovidal@gmail.com>
2025-04-16 11:11:38 +0200

    add cors

--- src/main/java/com/jsrom/fenw_items_jrv/FenwItemsJrvApplication.java
+++ src/main/java/com/jsrom/fenw_items_jrv/FenwItemsJrvApplication.java
@@ -2,10 +2,22 @@ package com.jsrom.fenw_items_jrv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FenwItemsJrvApplication {
+
+	    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").maxAge(3600);
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FenwItemsJrvApplication.class, args);
